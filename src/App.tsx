b6e3b74eaa05c5f7bc079663fedeeb2bfa9d4fab
lizj <1091820468@qq.com>
2020-07-15 17:53:42 +0800

    解决冲突

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,30 @@ import Button, { ButtonType, ButtonSize } from './components/Button/button';
 import Alert, { AlertType } from './components/Alert/alert';
 
 function App() {
-  const a = '222';
-  if(a === '222'){
-    console.log(a);
-  }
-
-  const handleClick = () => {
-    alert(1);
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Button>
-          hello
+        <div>
+          <Button>
+            hello
         </Button>
 
-        <Button onClick={handleClick} btnType={ButtonType.Primary} size={ButtonSize.Large}>
-          hello
+          <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+            hello
         </Button>
 
 
-        <Button btnType={ButtonType.Link} href="baidu.com">
-          hello
+          <Button btnType={ButtonType.Link} href="baidu.com">
+            hello
         </Button>
 
-        <Button btnType={ButtonType.Primary} disable href="baidu.com">
-          hello
+          <Button btnType={ButtonType.Primary} disable href="baidu.com">
+            hello
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
+        <br />
+        <Alert type={AlertType.Success} />
       </header>
     </div>
   );
