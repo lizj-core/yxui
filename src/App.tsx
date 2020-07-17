e333f799af297c168c1eb91ed28037de7fbaddb5
1091820468 <1091820468@qq.com>
2020-07-17 08:01:13 +0800

    'menu部分代码实现'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
 import Alert, { AlertType } from './components/Alert/alert';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   const a = '222';
@@ -15,6 +17,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu>
+          <MenuItem>
+            link1
+          </MenuItem>
+          <MenuItem>
+            link2
+          </MenuItem>
+          <MenuItem>
+            link3
+          </MenuItem>
+        </Menu>
         <Button>
           hello
         </Button>
