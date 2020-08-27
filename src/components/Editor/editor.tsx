55859c88cd3dcfd4994400f75e7bf42fc2244108
lizj <1091820468@qq.com>
2020-08-27 20:50:14 +0800

    docs: 提交changelog日志

--- src/components/Editor/editor.tsx
+++ src/components/Editor/editor.tsx
@@ -1,8 +1,9 @@
-import React,{ FC, useState, useEffect } from 'react';
+import React,{ FC, useState } from 'react';
 import 'braft-editor/dist/index.css'
 import BraftEditor from 'braft-editor'
 import useDebounce from '../../hooks/useDebounce'
-
+import Icon from '../Icon/icon'
+import Upload from '../Upload/upload';
 
 interface EditorProps {
   value?: string;
@@ -15,20 +16,46 @@ export const Editor: FC<EditorProps> = (props) => {
     onChange,
   } = props;
   const [editorState, setEditorState] = useState(BraftEditor.createEditorState(value || null))
-
+  
   const handleEditorChange = (editorState: any) => {
     setEditorState(editorState);
     if(onChange) {
       onChange(editorState.toHTML())
-      console.log(editorState.toHTML());
-      
     }
   }
+
+  const extendControls = [
+    {
+      key: 'custom-modal',
+      type: 'modal',
+      title:'word导入',
+      text: <Icon icon="address-book" />,
+      modal: {
+        id: 'my-moda-1',
+        title: 'word导入',
+        children: (
+          <div style={{ width: 400, padding: '0 10px' }}>
+            <Upload 
+              action="http://192.168.10.89/fs/api/v1.0/uploadFile" 
+              name="file"
+              multiple
+              drag
+            >
+              <Icon icon="upload" size="5x" theme="secondary" />
+              <br />
+              <p>Drag file over to upload</p>
+            </Upload>
+          </div>
+        )
+      }
+    }
+  ]
 
   return (
     <BraftEditor 
       value={editorState}
       onChange={handleEditorChange}
+      extendControls={extendControls}
     />
   )
 }
